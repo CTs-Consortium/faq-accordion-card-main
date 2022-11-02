73eb047ba96eb13599b2faedc2acaee39ec10882
Christopher Todd <ctsconsortium313@yahoo.com>
2022-11-02 14:01:13 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict'
+
+
+            ///////////FIRST TRY
+
+// accordionContent.forEach(accordionContent =>{
+// const accordionBody = document.querySelectorAll('.accordionBody')
+
+
+//     accordionContent.addEventListener('click', function(){
+//         accordionBody.forEach(accordionBody =>{
+//             accordionBody.classList.remove('hidden')
+//         })
+//     })
+// })
